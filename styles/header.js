d8585bf4ad2856636699104cc5846b7848c7998e
Leon Gaban <leongaban@gmail.com>
2019-01-31 16:43:35 -0600

    base styles added

---
+++ styles/header.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const HeaderStyle = styled.div`
+  h1 {
+    font-family: 'Oswald', sans-serif;
+    font-size: 3rem;
+  }
+`;
---
+++ styles/index.js
@@ -0,0 +1,7 @@
+import * as page from './page';
+import * as header from './header';
+
+export {
+  page,
+  header
+};
