900ff89e9485c2a4358cae568e2521fbc100c9b0
Leon Gaban <leongaban@gmail.com>
2019-02-01 15:44:08 -0600

    HOTFIX for merge into dev issue.
    - IRatesRes was using old code in the utils/converters file

--- utils/converters.ts
+++ utils/converters.ts
@@ -1,5 +1,5 @@
-import { IRatessRes } from '../store'
+import { IRatesRes } from '../shared/types'
 
 // Takes rates { key : value } pairs and converts into Array of objects.
-export const ratesIntoArray = ({ data: { rates } }: IRatessRes) =>
+export const ratesIntoArray = ({ data: { rates } }: IRatesRes) =>
   Object.keys(rates).map(data => new Object({ currency: data, rate: rates[data]}));
